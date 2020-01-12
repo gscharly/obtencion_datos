891b1c72cdd8bcadcda7a729f44ab600374ec42a
Paula <p.santamariav.2019@alumnos.urjc.es>
2020-01-12 20:18:04 +0100

    Fixed links cr

--- scraper.py
+++ scraper.py
@@ -15,7 +15,8 @@ def html(url):
 def obtener_link_lineas(url):
     soup = html(url)
     regex = re.compile('\/*aspx')
-    link_lineas = soup.find('div', attrs={'class': 'listaBotones logosCuadrado dosCols'}).find_all('a',attrs={'href': regex})
+    regex3 = re.compile('listaBotones *')
+    link_lineas = soup.find('div', attrs={'class': regex3}).find_all('a',attrs={'href': regex})
     patron = '\/.*aspx'
     link_lineas = [re.findall(patron, str(item)) for item in link_lineas]
     link_lineas = ['https://www.crtm.es/' + item[0] for item in link_lineas]
@@ -55,7 +56,7 @@ url_ml = 'https://www.crtm.es/tu-transporte-publico/metro-ligero/lineas.aspx'
 url_cr = 'https://www.crtm.es/tu-transporte-publico/cercanias-renfe/lineas.aspx'
 
 
-url_lista = obtener_link_lineas(url_ml)
+url_lista = obtener_link_lineas(url_cr)
 obtener_nombre_paradas(url_lista)
 obtener_orden_paradas(url_lista)
 obtener_numero_linea(url_lista)
